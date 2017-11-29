5897cc43d72872a6efaf263f118ba1483edb4b2d
mp_jr1 <mapg.jr@gmail.com>
2017-11-29 17:05:35 +0000

    Recipe model created

---
+++ projects/Course-Project/src/app/recipes/recipes.model.js
@@ -0,0 +1,10 @@
+"use strict";
+var Recipe = (function () {
+    function Recipe(name, desc, imagePath) {
+        this.name = name;
+        this.description = desc;
+        this.imagePath = imagePath;
+    }
+    return Recipe;
+}());
+exports.Recipe = Recipe;
